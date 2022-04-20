70f15cca661ed3dbd07d5fbf549356e037f14b8f
seokjin0404 <2017145088@yonsei.ac.kr>
2022-04-20 02:32:22 +0000

    [Feat] Upload augmentation.py Update dataset.py, train.py
    resolves #13

--- code/train.py
+++ code/train.py
@@ -19,11 +19,10 @@ import madgrad
 import adamp
 
 from east_dataset import EASTDataset
-from dataset import SceneTextDataset
+from dataset import SceneTextDataset, ValidSceneTextDataset
 from model import EAST
 import wandb
-
-import dataset_no_resize_no_crop
+from tqdm import tqdm
 
 def parse_args():
     parser = ArgumentParser()
@@ -31,6 +30,8 @@ def parse_args():
     # Conventional args
     parser.add_argument('--data_dir', type=str,
                         default=os.environ.get('SM_CHANNEL_TRAIN', '../input/data/ICDAR17_Korean'))
+    parser.add_argument('--val_data_dir', type=str,
+                        default=os.environ.get('SM_CHANNEL_TRAIN', '../input/data/camper'))
     parser.add_argument('--model_dir', type=str, default=os.environ.get('SM_MODEL_DIR',
                                                                         'trained_models'))
     parser.add_argument('--project', type=str, default = "data-annotation")
@@ -51,7 +52,7 @@ def parse_args():
     parser.add_argument('--optimizer', type=str, default = "Adam")
     parser.add_argument('--exp_name', type=str, default = "exp1")
 
-    parser.add_argument('--val_data', type=str, default = None)  # ufo 폴더 안에 있는 valid에 사용할 데이터의 json파일 이름, None일 경우 valid 안함
+    parser.add_argument('--val_data', type=str, default = "train")  # ufo 폴더 안에 있는 valid에 사용할 데이터의 json파일 이름, None일 경우 valid 안함
     parser.add_argument('--val_image_size', type=str, default = 512)
     parser.add_argument('--val_input_size', type=str, default = 512)
 
@@ -79,7 +80,7 @@ def set_seed(seed) :
     imgaug.random.seed(seed)
     print(f"seed : {seed}")
 
-def do_training(data_dir, model_dir, device, image_size, input_size, num_workers, batch_size,
+def do_training(data_dir, val_data_dir, model_dir, device, image_size, input_size, num_workers, batch_size,
                 learning_rate, max_epoch, save_interval, project, entity, name, seed, optimizer, exp_name,
                 val_data, val_image_size, val_input_size, load):
 
@@ -107,10 +108,11 @@ def do_training(data_dir, model_dir, device, image_size, input_size, num_workers
     train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, worker_init_fn=seed_worker)
 
     if val_data:
-        val_dataset = SceneTextDataset(data_dir, split=val_data, image_size=val_image_size, crop_size=val_input_size, color_jitter=False, valid=True)
-        val_dataset = EASTDataset(val_dataset)
+        val_dataset = ValidSceneTextDataset(val_data_dir, split='train', image_size=val_image_size, crop_size=val_input_size, color_jitter=False)
+        val_dataset.load_image()
+        print(f"Load valid data {len(val_dataset)}")
+        valid_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, collate_fn=ValidSceneTextDataset.collate_fn)
         val_num_batches = math.ceil(len(val_dataset) / batch_size)
-        val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, worker_init_fn=seed_worker)
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = EAST()
@@ -192,37 +194,71 @@ def do_training(data_dir, model_dir, device, image_size, input_size, num_workers
 
  ############validation########## 
         if val_data:
-            epoch_val_loss, epoch_val_Cls, epoch_val_Angle, epoch_val_IoU = 0, 0, 0, 0
-            model.eval()   
+            val_epoch_loss = 0
+            val_cls_loss = 0
+            val_angle_loss = 0
+            val_iou_loss = 0            
+            pred_bboxes_dict = dict()
+            gt_bboxes_dict = dict()
+            transcriptions_dict = dict()
             with tqdm(total=val_num_batches) as pbar:
-                for img, gt_score_map, gt_geo_map, roi_mask in val_loader:
-                    pbar.set_description('[Epoch {}] Val'.format(epoch + 1))
+                with torch.no_grad():
+                    model.eval()
+                    for step, (img, gt_score_map, gt_geo_map, roi_mask, vertices, orig_sizes, labels, transcriptions, fnames) in enumerate(valid_loader):
+                        pbar.set_description('[Valid {}]'.format(epoch + 1))
 
-                    loss, extra_info = model.train_step(img, gt_score_map, gt_geo_map, roi_mask)
-                    # optimizer.zero_grad()
-                    # loss.backward()
-                    # optimizer.step()
+                        loss, extra_info = model.train_step(img, gt_score_map, gt_geo_map, roi_mask)
 
-                    loss_val = loss.item()
-                    epoch_val_loss += loss_val
-                    epoch_val_Cls += extra_info['cls_loss']
-                    epoch_val_Angle += extra_info['angle_loss']
-                    epoch_val_IoU += extra_info['iou_loss']
+                        score_maps, geo_maps = extra_info['score_map'], extra_info['geo_map']
+                        score_maps, geo_maps = score_maps.cpu().numpy(), geo_maps.cpu().numpy()
 
-                    pbar.update(1)
-                    val_dict = {
-                        'Cls loss': extra_info['cls_loss'], 'Angle loss': extra_info['angle_loss'],
-                        'IoU loss': extra_info['iou_loss']
-                    }
-                    pbar.set_postfix(val_dict)
+                        by_sample_bboxes = []
+                        for i, (score_map, geo_map, orig_size, vertice, transcription, fname) in enumerate(zip(score_maps, geo_maps, orig_sizes, vertices, transcriptions, fnames)):
+                            map_margin = int(abs(orig_size[0] - orig_size[1]) * 0.25 * image_size / max(orig_size))
+                            if orig_size[0] > orig_size[1]:
+                                score_map, geo_map = score_map[:, :, :-map_margin], geo_map[:, :, :-map_margin]
+                            else:
+                                score_map, geo_map = score_map[:, :-map_margin, :], geo_map[:, :-map_margin, :]
 
-            print('Mean loss: {:.4f} | Elapsed time: {}'.format(
-                epoch_val_loss / val_num_batches, timedelta(seconds=time.time() - epoch_start)))
+                            bboxes = get_bboxes(score_map, geo_map)
+                            if bboxes is None:
+                                bboxes = np.zeros((0, 4, 2), dtype=np.float32)
+                            else:
+                                bboxes = bboxes[:, :8].reshape(-1, 4, 2)
 
-            wandb.log({ 'val / Mean Cls loss': epoch_val_Cls / val_num_batches, 
-                        'val / Mean Angle loss': epoch_val_Angle / val_num_batches,
-                        'val / Mean IoU loss': epoch_val_IoU / val_num_batches,
-                        'val / Mean_loss': epoch_val_loss / val_num_batches})
+                            pred_bboxes_dict[fname] = bboxes
+                            gt_bboxes_dict[fname] = vertice
+                            transcriptions_dict[fname] = transcription
+
+                        loss_val = loss.item()
+                        if loss_val is not None:
+                            val_epoch_loss += loss_val
+
+                        pbar.update(1)
+                        val_dict = {
+                            'Cls loss': extra_info['cls_loss'],
+                            'Angle loss': extra_info['angle_loss'],
+                            'IoU loss': extra_info['iou_loss']
+                        }
+                        pbar.set_postfix(val_dict)
+
+                        if val_dict['Cls loss'] is not None:
+                            val_cls_loss += val_dict['Cls loss']
+                            val_angle_loss += val_dict['Angle loss']
+                            val_iou_loss += val_dict['IoU loss']
+            resDict = calc_deteval_metrics(pred_bboxes_dict, gt_bboxes_dict, transcriptions_dict)
+
+            print('[Valid {}]: f1_score : {:.4f} | precision : {:.4f} | recall : {:.4f}'.format(
+                    epoch+1, resDict['total']['hmean'], resDict['total']['precision'], resDict['total']['recall']))
+
+            wandb.log({ "val/loss": val_epoch_loss / val_num_batches,
+                    "val/cls_loss": val_cls_loss / val_num_batches,
+                    "val/angle_loss": val_angle_loss / val_num_batches,
+                    "val/iou_loss": val_iou_loss / val_num_batches,
+                    "val/recall": resDict['total']['recall'],
+                    "val/precision": resDict['total']['precision'],
+                    "val/f1_score": resDict['total']['hmean'],
+                    "epoch":epoch+1})
 
 
         
